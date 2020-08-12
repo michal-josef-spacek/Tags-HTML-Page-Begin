df48f13d3e70acedc98d12f6c6c9cf6bba5a18b7
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-08-12 23:14:24 +0200

    Prepare for 0.07 version

--- Begin.pm
+++ Begin.pm
@@ -12,7 +12,7 @@ Readonly::Hash my %LANG => (
 	'title' => 'Page title',
 );
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
@@ -430,6 +430,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
